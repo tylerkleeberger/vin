2648233934219c67b4b1cd9c0f7f14a0629ad416
Jon Rista <jrista@users.noreply.github.com>
2023-05-02 15:14:37 -0600

    feat(apps): vinmaster
      + Added vinmaster ionic app
      * Updated capacitor config for new ionic app
      ^ Installed @nxext/ionic-angular package
      ^ Installed @nxext/capacitor package

---
+++ apps/vinmaster/android/app/src/main/java/com/test/vinmaster/MainActivity.java
@@ -0,0 +1,5 @@
+package com.test.vinmaster;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ apps/vinmaster/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.test.vinmaster',
+  appName: 'VINMaster',
+  webDir: '../../dist/apps/vinmaster',
+  bundledWebRuntime: false,
+};
+
+export default config;
